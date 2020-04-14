6bf95b59cd4f450b8d08147a7bbdc3b25181b8a8
Richard Veldman <Rveldman@pointlogic.com>
2020-04-14 12:41:31 +0200

    added webserver and poc md slide

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('.'))
+app.listen(3001, () => console.log('Slide show running on port 3000!'))
---
+++ slides/index.md
@@ -0,0 +1,16 @@
+### NMI Engine workshop
+
+
+
+## Images:
+
+![](img/target-screen.png)
+
+
+## Vertical slide
+
+
+
+## Last slide
+
+> with a quote to go
